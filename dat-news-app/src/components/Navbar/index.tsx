f0dc9f06017aea6db6c193f92f82e46b03e98e42
Deveesh Shetty <deveeshshetty@gmail.com>
2023-01-28 11:59:43 +0530

    Improved hover animation in navbar

--- dat-news-app/src/components/Navbar/index.tsx
+++ dat-news-app/src/components/Navbar/index.tsx
@@ -1,52 +1,44 @@
-import Link from 'next/link'
-import React from 'react'
-import {
-    useNavigation,
-} from "@pankod/refine-core";
-
+import Link from "next/link"
+import React from "react"
+import { useNavigation } from "@pankod/refine-core"
 
 const navigationItems = [
   {
-    title: 'Home',
-    href: '/',
+    title: "Home",
+    href: "/",
   },
   {
-    title: 'News',
-    href: '/news',
+    title: "News",
+    href: "/news",
   },
   {
-    title: 'About',
-    href: '/about',
+    title: "About",
+    href: "/about",
   },
   {
-    title: 'Contact',
-    href: '/contact',
-  }
+    title: "Contact",
+    href: "/contact",
+  },
 ]
 
 export default function index() {
-  const { list } = useNavigation();
+  const { list } = useNavigation()
   return (
     <div>
       <div className="hidden md:block">
-      <header className='flex flex-row items-center h-12 shadow-lg md:block'>
-   
-      <div className="flex flex-row gap-4 ml-12">
-          {
-            navigationItems.map((item, i) => (
-              <div className='hover:text-blue-500 hover:border-b-2 hover:border-blue-500 py-2
-              ' key={i}>
-                <Link href={item.href}>
-                  {item.title}
-                </Link>
+        <header className="flex flex-row items-center shadow-lg md:block">
+          <div className="flex flex-row gap-8 mx-12 py-4">
+            {navigationItems.map((item, i) => (
+              <div
+                className="list-item hover:text-blue-500 transition-colors duration-300"
+                key={i}
+              >
+                <Link href={item.href}>{item.title}</Link>
               </div>
-            ))
-          }
+            ))}
           </div>
-    
-            </header>
-           </div> 
-
+        </header>
+      </div>
     </div>
   )
 }
